38a37dd3a9eb885fabc47538ca3ccce707c8c740
Griffin Lacek <griffinlacek@gmail.com>
2016-04-07 01:32:54 -0500

    Working JSON Requests

--- horoscope.js
+++ horoscope.js
@@ -1,29 +1,74 @@
+
 var horoscopeSign = document.getElementById("horoscopeSign");
 var horoscopeDate = document.getElementById("horoscopeDate");
-var horoscope = document.getElementById("horoscope");
+var horoscopeText = document.getElementById("horoscope");
 
-var xhr = new XMLHttpRequest();
+var aries = document.getElementById("aries").onclick = function() {
+	getDailyHoroscope("aries");
+}
+var taurus = document.getElementById("taurus").onclick = function() {
+	getDailyHoroscope("taurus");
+}
+var gemini = document.getElementById("gemini").onclick = function() {
+	getDailyHoroscope("gemini");
+}
+var cancer = document.getElementById("cancer").onclick = function() {
+	getDailyHoroscope("cancer");
+}
+var leo = document.getElementById("leo").onclick = function() {
+	getDailyHoroscope("leo");
+}
+var virgo = document.getElementById("virgo").onclick = function() {
+	getDailyHoroscope("virgo");
+}
+var libra = document.getElementById("libra").onclick = function() {
+	getDailyHoroscope("libra");
+}
+var scorpio = document.getElementById("scorpio").onclick = function() {
+	getDailyHoroscope("scorpio");
+}
+var sagittarius = document.getElementById("sagittarius").onclick = function() {
+	getDailyHoroscope("sagittarius");
+}
+var capricorn = document.getElementById("capricorn").onclick = function() {
+	getDailyHoroscope("capricorn");
+}
+var aquarius = document.getElementById("aquarius").onclick = function() {
+	getDailyHoroscope("aquarius");
+}
+var pisces = document.getElementById("pisces").onclick = function() {
+	getDailyHoroscope("pisces");
+}
 
-window.onload = getDailyHoroscope("Aries");
+window.onload = getDailyHoroscope("aries");
+
 
 function getDailyHoroscope(sign) {
 	
-	var url = "http://horoscope-api.herokuapp.com/horoscope/today/" + sign;
+	var yql_url = 'https://query.yahooapis.com/v1/public/yql';
+	var url = "http://theastrologer-api.herokuapp.com/api/horoscope/" + sign + "/today";
 	
-	xhr.open('GET', url, true);
-	xhr.send();
-	
-	xhr.addEventListener("readystatechange", processRequest, false);
-	
-	xhr.onreadystatechange = processRequest;
-}
-
-function processRequest(e) {
-	if (xhr.readyState == 4 && xhr.status == 200) {
-		var response = JSON.parse(xhr.responseText);
+	var horoscope;
 		
-		horoscopeSign.innerHTML = response.sunsign;
-		horoscopeDate.innerHTML = response.date;
-		horoscope.innerHTML = response.horoscope;
-	}
+	$.ajax({
+		url: yql_url,
+		data: {
+			q: 'SELECT * FROM json WHERE url="'+url+'"',
+			format: 'json',
+			jsonCompat: 'new',
+		},
+		dataType: 'jsonp',
+		success: function(response){
+			console.log(response);
+			var date = response.query.results.json.date;
+			var horoscope = response.query.results.json.horoscope;
+			var sign = response.query.results.json.sunsign;
+
+			horoscopeSign.innerHTML = sign;
+			horoscopeDate.innerHTML = date;
+			horoscopeText.innerHTML = horoscope;
+			
+			}
+	})
+
 }
